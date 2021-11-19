27ddf0f86b9bf33b2727e1f04ebc645f677d5a92
quocbao19982009 <86527891+quocbao19982009@users.noreply.github.com>
2021-11-19 20:50:41 +0200

    changing css style

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,7 +1,7 @@
 import { Fragment } from "react";
 
 import HeaderCartButton from "./HeaderCartButton";
-import mealsImage from "../../assets/meals.jpg";
+import mealsImage from "../../assets/meal2.jpg";
 import classes from "./Header.module.css";
 import { Link } from "react-router-dom";
 const Header = (props) => {
@@ -22,9 +22,9 @@ const Header = (props) => {
           <HeaderCartButton onClick={props.onShowCart} />
         </div>
       </header>
-      <div className={classes["main-image"]}>
+      {/* <div className={classes["main-image"]}>
         <img src={mealsImage} alt="A table full of delicious food!" />
-      </div>
+      </div> */}
     </>
   );
 };
